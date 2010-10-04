23ab8d02b7eafd29bc9c067cbefea244c621f920
THAC0 <gunjam@gmail.com>
2010-10-04 23:10:06 +0100

    + Added ability to drag and drop banner fields in the widget settings making   it easy to reorder banners you already have.
    + You can now delete specific banners by clicking the X in the corner of the
      banner field.
    
    + Stylish new previous/next arrows for banner and gradiated progress bar.
    
    + Some CSS optimisations.

--- form.js
+++ form.js
@@ -2,24 +2,47 @@ $j = jQuery.noConflict();
 
 function addFormField() {
 
-	var FieldNo = $j('#sbt-container:first div').size();
+	var FieldNo = $j('#sbt-container:first li').size();
 
-	var fields = "<div id=\"sbt-" + FieldNo + "\"><p>Banner " + (FieldNo+1) + "</p>" +
+	var fields = "<li id=\"sbt-" + FieldNo + "\"><h3>Banner " + (FieldNo+1) + "<span onClick=\"removeFormField(" + FieldNo + ")\">&#215;</span></h3>" +
 		"<p><label for=\"sbt-image-" + FieldNo + "\">Image URL:</label>" +
 		"<input type=\"text\" id=\"sbt-image-" + FieldNo + "\" name=\"banner-" + FieldNo + "[image]\" /></p>" +
 		"<p><label for=\"sbt-post-" + FieldNo + "\">Post URL:</label>" +
-		"<input type=\"text\" id=\"sbt-post-" + FieldNo + "\" name=\"banner-" + FieldNo + "[post]\" /></p></div>";
+		"<input type=\"text\" id=\"sbt-post-" + FieldNo + "\" name=\"banner-" + FieldNo + "[post]\" /></p></li>";
 
 	$j("#sbt-container:first-child").append(fields);
-	$j("#sbt-container:last-child").append(fields);
+
+}
+function updateAttr(ele,attr) {
+
+	attr = ele.attr(attr);
+
+	if ( attr.substr(-7) == '[image]' ) return 'banner-'+ele.parent().parent().index()+'[image]';
+	else if ( attr.substr(-6) == '[post]' ) return 'banner-'+ele.parent().parent().index()+'[post]';
+	else if ( attr.substr(0,8) == 'sbt-post' ) return 'sbt-post-'+ele.parent().parent().index();
+	else return 'sbt-image-'+ele.parent().parent().index();
+
+}
+function updateForm() {
+	$j('#sbt-container:first-child li').attr("id",function(){return 'sbt-'+$j(this).index();});
+	$j('#sbt-container:first-child h3').text(function(){return 'Banner '+($j(this).parent().index()+1);});
+	$j('#sbt-container:first-child h3').append(function(){return "<span onClick=\"removeFormField("+$j(this).parent().index()+")\">&#215;</span>";});
+	$j('#sbt-container:first-child li label').attr("for",function(){return updateAttr($j(this),"for");});
+	$j('#sbt-container:first-child li input').attr("id",function(){return updateAttr($j(this),"id");});
+	$j('#sbt-container:first-child li input').attr("name",function(){return updateAttr($j(this),"name");});
+}
+function removeFormField(FieldNo) {
+
+	$j("#sbt-"+FieldNo).remove();
+	$j("#sbt-"+FieldNo).animate({height: 0}, 250, 'linear', function(){
+		$j(this).remove();
+		updateForm();
+	});
 
 }
 
-function removeFormField() {
-	
-	var FieldNo = $j("#sbt-container:last div").size()-1;
-	
-	$j("#sbt-"+FieldNo).remove();
-	$j("#sbt-"+FieldNo).remove();
+$j(document).ready(function(){ 
 
-}
+	$j("ul#sbt-container").sortable({ opacity: 0.6, placeholder: 'sbt-placeholder', forcePlaceholderSize: true, update: function(){updateForm();}});
+
+});
--- scrolly-banner-thing.php
+++ scrolly-banner-thing.php
@@ -4,7 +4,7 @@ Plugin Name: Scrolly Banner Thing
 Plugin URI: http://github.com/gunjam/Scrolly-Banner-Thing
 Description: Fade some massive images for some reason (with links!). 
 Author: Niall Molloy
-Version: 0.7
+Version: 0.8
 Author URI: http://purplejam.co.uk
 License: GPL v3
 */
@@ -21,15 +21,16 @@ function scrolly_banner_thing_init() {
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('banner');
 
-		wp_register_style('banner-style', '/'.PLUGINDIR.'/scrolly-banner-thing/banner.css', false, '0.2');
+		wp_register_style('banner-style', '/'.PLUGINDIR.'/scrolly-banner-thing/banner.css', false, '0.4');
 		wp_enqueue_style('banner-style');
 	}
 	else {
-		wp_register_script('form', '/'.PLUGINDIR.'/scrolly-banner-thing/form.js', array('jquery'), '1.3');
+		wp_register_script('form', '/'.PLUGINDIR.'/scrolly-banner-thing/form.js', array('jquery'), '1.5');
 		wp_enqueue_script('jquery');
+		wp_enqueue_script('jquery-ui-sortable');
 		wp_enqueue_script('form');
 
-		wp_register_style('form-style', '/'.PLUGINDIR.'/scrolly-banner-thing/form.css', false, '0.1');
+		wp_register_style('form-style', '/'.PLUGINDIR.'/scrolly-banner-thing/form.css', false, '0.2');
 		wp_enqueue_style('form-style');
 	}
 
@@ -88,9 +89,11 @@ function scrolly_banner_thing_preferences () {
 
 	if (isset($_POST['submit-settings'])) {
 		$options = array();
+		$i = 0;
 		foreach ( $_POST as $k => $v ) {
 			if ( substr($k,0,6) == 'banner' && $v ) {
-				$options[$k] = $v;
+				$options["banner-$i"] = $v;
+				$i++;
 			}
 		}
 		update_option ( $SBT_PLUGIN_NAME, $options );
@@ -99,18 +102,17 @@ function scrolly_banner_thing_preferences () {
 		$options = get_option ($SBT_PLUGIN_NAME);
 	}
 
-	$form = "<div id=\"sbt-container\"><input type=\"hidden\" name=\"submit-settings\" value=\"1\" />";	
+	$form = "<ul id=\"sbt-container\">";	
 	
 	for ( $i = 0; $i < count($options); $i++ ) {
 		$form .= "
-			<div id=\"sbt-$i\"><h3>Banner ".($i+1)."</h3><p><label for=\"sbt-image-$i\">Image URL:</label>
+			<li id=\"sbt-$i\"><h3>Banner ".($i+1)."<span onClick=\"removeFormField($i)\">&#215;</span></h3><p><label for=\"sbt-image-$i\">Image URL:</label>
 			<input type=\"text\" id=\"sbt-image-$i\" name=\"banner-{$i}[image]\" value=\"" . $options["banner-$i"]['image'] . "\" /></p>
 			<p><label for=\"sbt-post-$i\">Post URL:</label>
-			<input type=\"text\" id=\"sbt-post-$i\" name=\"banner-{$i}[post]\" value=\"" . $options["banner-$i"]['post'] . "\" /></p></div>
+			<input type=\"text\" id=\"sbt-post-$i\" name=\"banner-{$i}[post]\" value=\"" . $options["banner-$i"]['post'] . "\" /></p></li>
 		";
 	}
 
-	echo $form."</div><p><a id=\"sbt-add\" title=\"Add another banner fieldset\" href=\"#sbt-add\" onClick=\"addFormField()\">Add</a> /
-	<a id=\"sbt-rem\" title=\"Remove last banner fieldset\" href=\"#sbt-rem\" onClick=\"removeFormField()\">Remove</a> a banner</p>";
+	echo $form."</ul><p><a id=\"sbt-add\" title=\"Add another banner fieldset\" href=\"#sbt-add\" onClick=\"addFormField()\">Add</a> a banner</p><input type=\"hidden\" name=\"submit-settings\" value=\"1\" />";
 }
 ?>
